fix ndvi calculation

src/pipeline/mat.py
@@ -56,6 +56,7 @@ class Mat():
     Returns:
         The loaded Mat.
     """
+    # TODO: Warning - untested
     @classmethod
     def fread(cls, paths: dict[str, list[Channels]]) -> Mat:
         # Load the images
@@ -72,20 +73,19 @@ class Mat():
         channels = sum(paths.values(), [])
 
         # Combine arrays
-        arr = np.concatenate([np.asarray(mat[:,:]) for mat in mats], axis=0)
-        shape.append(len(channels))
+        arr = np.concatenate([np.asarray(mat[:,:,:]) for mat in mats], axis=2)
 
         # Return the combined data
-        return cls(shape, data=data, channels=channels)
+        return cls(shape, arr, channels=channels)
 
     """
-    Returns the underlying mat.
+    Provides a view of the underlying image array.
 
     Returns:
-        The underlying mat
+        A view of the underlying image array.
     """
     def get(self) -> cv2.Mat:
-        return self.arr
+        return self.arr.view()
     
     """
     Provides the specified channels of the mat.
@@ -94,7 +94,7 @@ class Mat():
         key: channel or list of channels to return
 
     Returns:
-        Ndarray of the specified channels.
+        Mat with the specified channels.
     """
     def __getitem__(self, key: Channels | list[Channels]) -> Mat:
         # Make sure the input is an array
@@ -102,8 +102,9 @@ class Mat():
             key = [key]
 
         # Get data
+        print([self.channels.index(channel) for channel in key])
         arr = self.arr[:,:,[self.channels.index(channel) for channel in key]]
 
-        # Return the new mat
+        # Otherwise, return the new mat
         return Mat(arr, channels=key)
 

src/pipeline/modules/index/runnables/ndvi.py
@@ -2,6 +2,7 @@ from ...runnable import Runnable
 from ...data import Data, Modules
 from ..indicies import Indicies
 from mat import Channels
+import numpy as np
 
 class NDVI(Runnable):
     def __init__(self, data: Data):
@@ -13,14 +14,35 @@ class NDVI(Runnable):
     """
     def run(self, data: Data) -> bool:
         try:
-            nir, red = data.modules[Modules.MOSAIC]["stitched"][[Channels.NIR, Channels.R]]
-            ndvi = (nir - red) / (nir + red)
+            # Get the channels
+            nir = data.modules[Modules.MOSAIC]["stitched"][Channels.NIR].get()
+            red = data.modules[Modules.MOSAIC]["stitched"][Channels.R].get()
+
+            # Calculate 
+            ndvi = self.calculate(nir, red)
             data.modules[Modules.INDEX]["runnables"][self.type]["index"] = ndvi
             return True
+
+        # Catch exception
         except Exception as e:
             print("NDVI calculation failed!")
             print(e)
             return False
+
+    """
+    Calulcates the NDVI index.
+
+    Args:
+        nir: the near-infrared spectrum
+        red: the red band
+
+    Returns:
+        The NDVI index.
+    """
+    def calculate(self, nir, red) -> np.ndarray:
+        a = np.asarray(nir - red, dtype=np.float64)
+        b = np.asarray(nir + red, dtype=np.float64)
+        return np.divide(a, b, out=np.zeros_like(a), where=b!=0, casting="unsafe")
 
     """
     Prepares the NDVI data space.