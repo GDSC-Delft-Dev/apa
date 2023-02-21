2f2ab4cb5a1f95ac0b789ec4402eec61fa2c895c
paulmis <chugdevs@gmail.com>
2023-02-21 03:15:54 +0100

    fix pylint (1/)

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -1,22 +1,25 @@
-from modules.module import Module
-from modules.mosaicing import Mosaicing
-from modules.segmentation import SemanticSegmentation
-import cv2
-from mat import Mat
-from modules.data import Data
-from config import Config
 import copy
 import numpy as np
 from typing import Any, Type
+from modules.module import Module
+from modules.mosaicing import Mosaicing
+from modules.segmentation import SemanticSegmentation
+from mat import Mat
+from modules.data import Data
+from config import Config
 
 class Pipeline:
     """
-    Build the pipeline according to the configuration.
+    The image processing pipeline object. Pipelines take in images and process them
+    with the modules specified in their configuration.
     """
+
     def __init__(self, config: Config):
+        """Build the pipeline according to the configuration."""
+
         # Build the data object
         self.data_proto: Data = Data()
-        
+
         # Build the head
         kv = next(iter(config.modules.items()))
         self.head: Module = kv[0](self.data_proto, input=kv[1])
@@ -27,22 +30,23 @@ class Pipeline:
             tail.next = module(self.data_proto, input=input)
             tail = tail.next
 
-    """
-    Runs the pipeline on the provided input images.
-
-    Args:
-        img: the input image(s)
-        rest: non-specific module arguments
-
-    Returns:
-        The processed data.
-    """
     def run(self, imgs: Mat | list[Mat]) -> Data:
+        """
+        Runs the pipeline on the provided input images.
+
+        Args:
+            img: the input image(s)
+            rest: non-specific module arguments
+
+        Returns:
+            The processed data.
+        """
+
         # Verify input integrity
         # Check that the channels of all images are the same
         if not isinstance(imgs, Mat):
             channels = [img.channels for img in imgs]
-            assert(channels.count(channels[0]) == len(channels))
+            assert channels.count(channels[0]) == len(channels)
 
         # Construct input data
         data = copy.deepcopy(self.data_proto)
@@ -51,11 +55,10 @@ class Pipeline:
         # Run the chain
         self.head.run(data)
         return data
-        
-    """
-    Prints out the current state of the pipeline.
-    """
+
     def show(self):
+        """Prints out the current state of the pipeline."""
+
         print("-- Pipeline --")
         tail = self.head
         while tail:
