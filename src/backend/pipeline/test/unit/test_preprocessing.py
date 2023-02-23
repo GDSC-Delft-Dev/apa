e4e6fc055e6e7c29c717c02f641864877b0adb13
mirceatlx <mirceatlx@gmail.com>
2023-02-23 22:02:55 +0100

    unit testing
    (cherry picked from commit 1e342a6d59de268eebab040db12da72727fa24f6)

---
+++ src/backend/pipeline/test/unit/test_preprocessing.py
@@ -0,0 +1,28 @@
+from ...modules.preprocess import Preprocess
+from ...modules.data import Data
+from ...mat import Mat
+from ...modules.types import Modules
+import glob
+import cv2
+import numpy as np
+
+class TestPreprocessingModule:
+    """
+    Unit testing for the preprocessing module.
+    """
+
+    def test_preprocessing(self):
+        """
+        Test the method run.   
+        """  
+        masks = [cv2.imread(file) for file in glob.glob("./test/data/mosaicing/farm/mask*.JPG")]
+        imgs = [Mat.read(file) for file in glob.glob("test/data/mosaicing/farm/D*.JPG")]
+        data: Data = Data()
+        data.modules[Modules.PREPROCESS] = {}
+        module = Preprocess(data, masks)
+        data.set(imgs)
+        module.run(data)
+        assert data.modules[Modules.PREPROCESS]["masked"] is not None
+        out = np.array([x.get() for x in data.modules[Modules.PREPROCESS]["masked"]])
+        expected = np.load("expected_preprocess_masked.npy", allow_pickle=True) 
+        assert np.array_equal(out, expected)
