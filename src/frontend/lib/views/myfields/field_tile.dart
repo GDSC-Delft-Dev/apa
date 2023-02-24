e51a4c95ed8a4d9c75981fe7d874593358180427
Nadine Kuo <hnkuo2@illinois.edu>
2023-02-24 12:12:55 +0100

    add field tiles in listview

---
+++ src/frontend/lib/views/myfields/field_tile.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/models/field_model.dart';
+
+class FieldTile extends StatelessWidget {
+
+  final FieldModel field;
+
+  FieldTile ({required this.field});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(top: 8.0),
+        child: Card(
+          elevation: 5,
+          margin: EdgeInsets.fromLTRB(20, 6, 20, 0),
+          child: ListTile(
+            leading: Icon(Icons.map_outlined),
+            // TODO: display crop
+            // TODO: add crop type icon or map of field
+            title: Text(field.fieldName),
+            subtitle: Text('${field.area} ha'),
+            trailing: Icon(Icons.keyboard_arrow_right_sharp),
+          )
+        )
+    );
+  }
+}
--- src/frontend/lib/views/myfields/fields_list.dart
+++ src/frontend/lib/views/myfields/fields_list.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:frontend/models/field_model.dart';
 import 'package:provider/provider.dart';
 
+import 'field_tile.dart';
+
 /// This widget is used within the "My Fields" screen to list
 class FieldList extends StatefulWidget {
 
@@ -16,15 +18,14 @@ class _FieldListState extends State<FieldList> {
   @override
   Widget build(BuildContext context) {
 
+    // Gets StreamProvider instances (from parent widget my_fields.dart) - Firebase documents of fields
     final fields = Provider.of<List<FieldModel>>(context);
 
-    return Scaffold(
-      body: Column(
-        children: [
-          // TODO: create tiles for all fields to display (see issue #16)
-          for (var field in fields) Center(child: Text(field.fieldName))
-        ],
-      ),
+    return ListView.builder(
+        itemCount: fields.length,
+        itemBuilder: (context, index) {
+          return FieldTile(field: fields[index]);
+        },
     );
   }
 
