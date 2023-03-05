82271c2f28ba2b9a0a54358b499d21c3b840ea5a
mirceatlx <mirceatlx@gmail.com>
2023-03-05 13:56:09 +0100

    add stitched test

--- src/backend/pipeline/test/unit/test_mosaicing.py
+++ src/backend/pipeline/test/unit/test_mosaicing.py
@@ -33,3 +33,24 @@ class TestMosaicingModule:
         mask = np.where(collapsed == 0.0, 1, 0)
         mask = np.expand_dims(mask, 2)
         assert np.array_equal(np.where(mask == 1, 0, 1), result.modules[Modules.MOSAIC]["mask"])
+
+    def test_mosaicing(self):
+        """
+        Test the method run of the mosaic module.
+        """
+        cfg = Config(modules={Mosaicing: None})
+        pipeline = Pipeline(cfg)
+        imgs = [Mat.read(file) for file in glob.glob("../data/mosaicing/farm/D*.JPG")]
+        # run the pipeline
+        result = pipeline.run(imgs)
+        imgs_shape = [mat.get().shape for mat in imgs]
+        # add up all the shapes
+        sumx = [0, 0]
+        for s in imgs_shape:
+            sumx[0] += s[0]
+            sumx[1] += s[1]
+        stitched_shape = result.modules[Modules.MOSAIC]["stitched"].get().shape
+        assert stitched_shape[0] <= sumx[0]
+        assert stitched_shape[1] <= sumx[1]
+        # only have RGB channels
+        assert stitched_shape[2] == 3
