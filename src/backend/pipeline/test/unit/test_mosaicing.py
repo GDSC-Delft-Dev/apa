08865d53d93126be30cc0d6da3fde1460bf55204
mirceatlx <mirceatlx@gmail.com>
2023-03-12 16:22:38 +0100

    fix mosaic tests

--- src/backend/pipeline/test/unit/test_mosaicing.py
+++ src/backend/pipeline/test/unit/test_mosaicing.py
@@ -26,14 +26,14 @@ class TestMosaicingModule:
         imgs = [Mat.read(file) for file in glob.glob("../data/mosaicing/farm/D*.JPG")]
         # run the pipeline with the Mosaicing module
         result = await pipeline.run(imgs)
-        assert result.modules[Modules.MOSAIC]["mask"] is not None
-        assert result.modules[Modules.MOSAIC]["alpha_img"] is not None
+        assert result.modules[Modules.MOSAIC.value]["mask"] is not None
+        assert result.modules[Modules.MOSAIC.value]["alpha_img"] is not None
 
-        stitched = result.modules[Modules.MOSAIC]["stitched"]
+        stitched = result.modules[Modules.MOSAIC.value]["stitched"]
         collapsed = np.sum(stitched.get(), axis=2)
         mask = np.where(collapsed == 0.0, 1, 0)
         mask = np.expand_dims(mask, 2)
-        assert np.array_equal(np.where(mask == 1, 0, 1), result.modules[Modules.MOSAIC]["mask"])
+        assert np.array_equal(np.where(mask == 1, 0, 1), result.modules[Modules.MOSAIC.value]["mask"])
 
     @pytest.mark.asyncio
     async def test_mosaicing_dimensions(self):
@@ -54,7 +54,7 @@ class TestMosaicingModule:
             sumx[1] += _shape[1]
             min_x = min(min_x, _shape[0])
             min_y = min(min_y, _shape[1])
-        stitched_shape = result.modules[Modules.MOSAIC]["stitched"].get().shape
+        stitched_shape = result.modules[Modules.MOSAIC.value]["stitched"].get().shape
         assert min_x <= stitched_shape[0] and stitched_shape[0] <= sumx[0]
         assert min_y <= stitched_shape[1] and stitched_shape[1] <= sumx[1]
         # only have RGB channels
