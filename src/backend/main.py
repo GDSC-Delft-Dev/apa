692ec0b2ae1a67f1408ce9f42c727f3789bc70c4
mirceatlx <mirceatlx@gmail.com>
2023-03-13 19:03:22 +0100

    add manual triggers script

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,12 +1,43 @@
 import glob
+import sys
+import os
+import argparse
 from pipeline.templates import full_pipeline, default_pipeline
 from pipeline.mat import Mat
+from google.cloud import storage
 
 def main():
     """Main entry point."""
 
-    # Get test data
-    imgs = [Mat.read(file) for file in glob.glob("pipeline/test/data/mosaicing/farm/D*.JPG")]
+    # parse arguments
+    parser = argparse.ArgumentParser(description='Process cloud arguments')
+    parser.add_argument('--path', dest='path', default=None)
+    parser.add_argument('--mode', dest='mode', default=None)
+    namespace = parser.parse_args(sys.argv[1:])
+    path = namespace.path
+    mode = namespace.mode
+
+    # check if we use cloud data or local data
+    if path is None or mode is None:
+        imgs = [Mat.read(file) for file in glob.glob("pipeline/test/data/mosaicing/farm/D*.JPG")]
+    else:
+        if not os.path.exists("./pipeline/data"):
+            os.mkdir("./pipeline/data/")
+        storage_client = storage.Client()
+        if mode == "local":
+            bucket = storage_client.get_bucket("terrafarm-inputs")
+        else:
+            bucket = storage_client.get_bucket("terrafarm-test")
+
+        blobs=bucket.list_blobs(prefix=path + "/D", delimiter="/")
+        for blob in blobs:
+            print(blob)
+            print(os.getcwd())
+            filename = f"./pipeline/data/{blob.name.split('/')[-1]}"
+            open(filename, 'a').close()
+            blob.download_to_filename(f"./pipeline/data/{blob.name.split('/')[-1]}")
+        
+        imgs = [Mat.read(file) for file in glob.glob("pipeline/data/D*.JPG")]
 
     # Run the pipeline
     pipeline = default_pipeline()
---
+++ src/backend/trigger.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#
+# Enable manual Google Cloud Run triggers for specific inputs 
+
+# install requirements
+
+gcloud components install beta
+
+PROJECT="terrafarm-378218"
+REGION="europe-west4"
+REPO="pipelines-dev"
+IMAGE="latest"
+
+VERSION=$(gcloud artifacts docker images list \
+${REGION}-docker.pkg.dev/${PROJECT}/${REPO} \
+--filter="package=${REGION}-docker.pkg.dev/${PROJECT}/${REPO}/${IMAGE}" \
+--sort-by="~UPDATE_TIME" \
+--limit=1 \
+--format="value(format("{0}@{1}",package,version))")
+
+echo "Latest artifact image $VERSION"
+
+# create the actual job:
+# JOB_NAME: the name of the job you want to create
+# IMAGE_URL: reference to a container image from the Artifact Registry
+# OPTIONS (any of the following): 
+#               --tasks: 	Accepts integers greater or equal to 1
+#               --max-retries: The number of times a failed task is retried
+#               --task-timeout: Accepts a duration like "2s". Defaults to 10 minutes; maximum is 1h. 
+#               --parallelism: The maximum number of tasks that can execute in parallel.
+#               --execute-now: If set, immediately after the job is created, a job execution is started. 
+#
+
+
+while getopts ":l:c:v:n:" opt; do
+  case $opt in
+    l) local_source="$OPTARG"
+    ;;
+    c) cloud_source="$OPTARG"
+    ;;
+    v) VERSION="$OPTARG"
+    ;;
+    n) job_name="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+echo "$local_source"
+
+# check if both local source and cloud source have been specified
+if [[ -n "$local_source" && -n "$cloud_source" ]]; then 
+  echo "You specified both local source and cloud source. You should onky specify one."
+  exit 1
+fi
+
+
+if [ -n "$local_source" ]; then
+  uuid=$(uuidgen)
+  uuid=$(echo "$uuid" | tr '[:upper:]' '[:lower:]')
+  echo "Created UUID for the local input data: $uuid"
+  gsutil -m cp -r $local_source gs://terrafarm-inputs/$uuid/
+  IFS='/' read -ra my_list <<< "$local_source"
+  cloud_path="$uuid/${my_list[-1]}"
+  mode="local"
+
+fi
+
+if [ -n "$cloud_source" ]; then
+  cloud_path=$cloud_source
+  mode="cloud"
+fi
+
+parse_path="--path $cloud_path"
+parse_mode="--mode $mode"
+
+gcloud beta run jobs create $job_name --image $VERSION --command=python,main.py --args="$parse_path","$parse_mode"
+echo "Job $job_name created"
+# execute job
+
+gcloud beta run jobs execute $job_name
+
+echo "Job $job_name finished" 
