bc8b5a7bbf19d2fb10a72a846cb9768f2e0f85c3
Nadine Kuo <hnkuo2@illinois.edu>
2023-03-17 17:09:52 +0100

    cloud function for fetching scan

--- src/frontend/functions/src/index.ts
+++ src/frontend/functions/src/index.ts
@@ -14,3 +14,23 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 exports.checkHealth = functions.https.onCall(async (data, context) => {
   return "The function is online";
 });
+
+// Fetches scan for which scanId matches
+// This can entry may contain urls to images for NDVI or RGB maps
+exports.fetchScan = functions.https.onCall(async (data, context) => {
+  const scanId = data.scanId;
+  // Verify user is authenticated
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "unauthenticated", "User is not authenticated.");
+  }
+  // Fetch scan document from Firestore
+  const scanDoc = await admin.firestore().collection("scans").doc(scanId).get();
+  if (!scanDoc.exists) {
+    throw new functions.https.HttpsError(
+      "not-found", "Scan document not found.");
+  }
+  // Return the scan data
+  const scanData = scanDoc.data();
+  return {scanData};
+});
--- src/frontend/lib/services/cloud_functions_service.dart
+++ src/frontend/lib/services/cloud_functions_service.dart
@@ -10,4 +10,26 @@ class CloudFunctionsService {
     }
   }
 
+  // Define a function to fetch a Scan entry from the "scans" collection
+  Future<Map<String, dynamic>> fetchScanNDVIMap(String scanId) async {
+
+    // Create a reference to the Firebase Cloud Functions instance
+    final HttpsCallable fetchMapCallable = FirebaseFunctions.instance.httpsCallable("fetchScan");
+
+    // Call the cloud function with the scanId parameter
+    final result = await fetchMapCallable.call({'scanId': scanId});
+
+    // Parse the result data as a Map and return it
+    return Map<String, dynamic>.from(result.data);
+  }
+
+  // Example usage
+// void exampleUsage() async {
+//   final scanId = 'ABC123'; // Replace with your scan ID
+//   final scanData = await fetchScan(scanId);
+//   print(scanData);
+// }
+  
+    
+
 }
