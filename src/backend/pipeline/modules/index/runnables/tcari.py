802af3918d547814071838ef0695b8acf41b156c
paulmis <chugdevs@gmail.com>
2023-03-20 20:04:58 +0100

    add TCARI index

---
+++ src/backend/pipeline/modules/index/runnables/tcari.py
@@ -0,0 +1,48 @@
+from ...data import Data
+from ..indicies import Indicies
+from ....mat import Mat, Channels
+import numpy as np
+from .generic import GenericIndex
+
+class NDVI(GenericIndex):
+    """
+    The Normalized Differential Vegetation Index runnable.
+    https://en.wikipedia.org/wiki/Normalized_difference_vegetation_index
+    """
+
+    def __init__(self, data: Data):
+        """
+        Initializes the NDVI runnable.
+
+        Args:
+            data: the pipeline data object
+        """
+        super().__init__(data, name="NDVI", color_map="RdYlGn",
+                         channels=[Channels.RE, Channels.G, Channels.R], 
+                         index_type=Indicies.GCI)
+
+    def calculate(self, img: Mat) -> np.ndarray:
+        """
+        Calulcates the NDVI index.
+        
+        Args:
+            img: the image to calculate the index on
+            
+        Returns:
+            The NDVI index.
+        """
+
+        re = img[Channels.RE].get()
+        red = img[Channels.R].get()
+        green = img[Channels.G].get()
+
+        a = (re - red) - 0.2
+        b = (re - green)
+        c_numerator = np.asarray(re, dtype=np.float64)
+        c_denominator = np.asarray(red, dtype=np.float64)
+        c = np.divide(c_numerator, c_denominator,
+                      out=np.zeros_like(c_numerator),
+                      where=c_denominator!=0,
+                      casting="unsafe")
+        
+        return 3 * a * b * c
