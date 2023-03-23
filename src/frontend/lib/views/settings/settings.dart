28fc82f11fd3a618b0853cbe07e11d4a5ebbc401
David Dinucu-Jianu <D.dinucu-jianu@student.tudelft.nl>
2023-03-23 23:16:37 +0100

    added app icon

--- src/frontend/lib/views/settings/settings.dart
+++ src/frontend/lib/views/settings/settings.dart
@@ -24,88 +24,109 @@ class _SettingsState extends State<Settings> {
         backgroundColor: Colors.white,
       ),
       backgroundColor: Colors.grey[200],
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // A list of settings (about and logout) they should be clickable.
-          // When clicked, they should perform the action
-          // For example, when the user clicks on logout, they should be logged out
-          // and taken to the login screen
-          GestureDetector(
-            onTap: () {
-              // Opens a bottom modal pop up with information about the app
-              showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 20,
-                      horizontal: 60,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+      
+            Row(children: [
+              // Image of the app logo
+              const SizedBox(width: 20),
+              Image.asset(
+                'assets/icons/logo.png',
+                height: 150,
+                width: 150,
+              ),
+              const SizedBox(width: 20),
+              // Text of the app name
+              Text(
+                'Terrafarm',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+      
+                ),
+              ),
+            ],),
+            
+            
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Opens a bottom modal pop up with information about the app
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 60,
+                      ),
+                      child: Text(
+                        'This app was created by the team at the TU Delft for the 2022-2023 Google Solutions Challenge. It was created to help farmers get automated insights about their fields and crops.',
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Container(
+                color: Colors.blue,
+                width: double.infinity,
+                height: 50,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.info,
+                      color: Colors.white,
                     ),
-                    child: Text(
-                      'This app was created by the team at the TU Delft for the 2022-2023 Google Solutions Challenge. It was created to help farmers get automated insights about their fields and crops.',
-                      style: const TextStyle(
-                        fontSize: 18,
+                    const SizedBox(width: 10),
+                    const Text(
+                      'About',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
                       ),
                     ),
-                  );
-                },
-              );
-            },
-            child: Container(
-              color: Colors.blue,
-              width: double.infinity,
-              height: 50,
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.info,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(width: 10),
-                  const Text(
-                    'About',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-
-          GestureDetector(
-            onTap: () async {
-              await _auth.signOut();
-            },
-            child: Container(
-              color: Colors.red,
-              width: double.infinity,
-              height: 50,
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.logout,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(width: 10),
-                  const Text(
-                    'Logout',
-                    style: TextStyle(
+      
+            GestureDetector(
+              onTap: () async {
+                await _auth.signOut();
+              },
+              child: Container(
+                color: Colors.red,
+                width: double.infinity,
+                height: 50,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.logout,
                       color: Colors.white,
-                      fontSize: 20,
                     ),
-                  ),
-                ],
+                    const SizedBox(width: 10),
+                    const Text(
+                      'Logout',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
